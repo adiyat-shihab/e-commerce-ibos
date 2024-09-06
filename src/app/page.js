f98c8df2339f794f9775e31ed02233779dc4a071
adiyat-shihab <mohammadshihab104@gmail.com>
2024-09-06 17:27:51 +0600

    fix navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
+import { useEffect } from "react";
+
 const Page = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const user = localStorage.getItem("user-session");
+    if (user) {
+      router.push("/dashboard");
+    } else {
+      router.push("/authentication/login");
+    }
+  }, []);
   return <div></div>;
 };
 
