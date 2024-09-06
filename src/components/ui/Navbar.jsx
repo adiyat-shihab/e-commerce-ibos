9f7825f4d874ea1bf5f3a05eaed5c9cfca24aa66
adiyat-shihab <mohammadshihab104@gmail.com>
2024-09-06 10:34:40 +0600

    change padding

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -15,14 +15,14 @@ const Navbar = () => {
   const { logOut } = useAuth();
 
   return (
-    <div className="flex items-center justify-between p-4 bg-white">
+    <div className="flex items-center justify-between p-4 bg-white border-b py-6">
       <div className="flex items-center">
         <div className="bg-blue-500 text-white rounded-full p-2 mr-2">
           <span className="text-xl font-bold">F</span>
         </div>
         <span className="text-xl font-bold">FurniFlex</span>
       </div>
-      <div className="hidden md:flex space-x-4 text-black hover:text-gray-900 text-xl font-medium">
+      <div className="hidden md:flex space-x-4 text-black hover:text-gray-900 text-xl gap-12 font-medium">
         <a href="#">Home</a>
         <a href="#">Products</a>
         <a href="#">Categories</a>
